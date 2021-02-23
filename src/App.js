451b8939267f1dc181d8f1bb3ac81dcc9f003789
Mliaqat <45541170+Mliaqat@users.noreply.github.com>
2021-02-23 08:55:50 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Uni from './uni.js'
+
+function App() {
+  return (
+    <Uni name="liaqat" class="BSSE5" />
+  );
+}
+
+export default App;
---
+++ src/uni.js
@@ -0,0 +1,17 @@
+    import React from 'react';
+
+    function Uni(props) {
+
+        return(
+
+            <div>
+
+           <h1> My is {props.name} and class is {props.class} </h1>
+
+            </div>
+
+        );
+        
+    }
+
+    export default Uni;
