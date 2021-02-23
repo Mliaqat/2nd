b0fb9ae880d287253b37f4e2286fbe34285f8868
Mliaqat <45541170+Mliaqat@users.noreply.github.com>
2021-02-23 10:41:49 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import Uni from './uni.js'
 
 function App() {
+
+  let [count , setcount] = useState(1);
+
   return (
+    <div>
     <Uni name="liaqat" class="BSSE5" />
+
+    <br></br>
+<h2>the vale of varaible is : {count}</h2>
+
+<br />
+
+<button onClick={()=>setcount(count+1)}>Presss</button>
+
+    </div>
   );
 }
 
--- src/uni.js
+++ src/uni.js
@@ -1,10 +1,16 @@
-    import {React} from "react";
+    import {React, useState} from "react";
 
     function  Uni(props) {
 
+        
+
         return(
 
+            <div>
             <h1>hi i am {props.name }in class {props.class}</h1>
+
+
+</div>
         );
         
     }
